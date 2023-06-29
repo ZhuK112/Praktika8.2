ecf3cbdf1b339edff99f902fcc5863c03e634607
ZhuK <127308032+ZhuK112@users.noreply.github.com>
2023-06-29 21:50:41 +0300

    Update result.php

---
+++ result.php
@@ -0,0 +1,20 @@
+<?php
+$a = $_GET['a'];
+$b = $_GET['b'];
+
+// Перевірка, чи введені числа є цілими
+if (!is_numeric($a) || !is_numeric($b) || !is_int($a + 0) || !is_int($b + 0)) {
+    echo "Введені значення повинні бути цілими числами.";
+    exit;
+}
+
+$sum = $a + $b;
+$product = $a * $b;
+$difference = $a - $b;
+$quotient = $a / $b;
+
+echo "Сума: " . $sum . "<br>";
+echo "Добуток: " . $product . "<br>";
+echo "Різниця: " . $difference . "<br>";
+echo "Частка: " . $quotient . "<br>";
+?>
